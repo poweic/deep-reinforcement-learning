556fb7045117b8a6742c9a9dc8e29560067e0d3e
Po-Wei Chou <poweic@andrew.cmu.edu>
2016-12-18 16:23:11 -0500

    Runnable. But need to use imshow to see whether it works

---
+++ go.py
@@ -0,0 +1,343 @@
+#!/usr/bin/python
+import sys
+import scipy.io
+import itertools
+import numpy as np
+import tensorflow as tf
+import collections
+
+from gym_offroad_nav.envs import OffRoadNavEnv
+from lib import plotting
+
+class VehicleModel():
+
+    def __init__(self, timestep=0.01):
+        model = scipy.io.loadmat("../vehicle_modeling/vehicle_model_ABCD.mat")
+        self.A = model["A"]
+        self.B = model["B"]
+        self.C = model["C"]
+        self.D = model["D"]
+        self.timestep = timestep
+
+        print "A: {}, B: {}, C: {}, D: {}".format(
+            self.A.shape, self.B.shape, self.C.shape, self.D.shape)
+
+        # x is the unobservable hidden state, y is the observation
+        # u is (v_forward, yaw_rate), y is (vx, vy, w), where
+        # vx is v_slide, vy is v_forward, w is yaw rate
+        # x' = Ax + Bu (prediction)
+        # y' = Cx + Du (measurement)
+        self.x = None
+
+    def _predict(self, x, u):
+        y = np.dot(self.C, x) + np.dot(self.D, u)
+        x = np.dot(self.A, x) + np.dot(self.B, u)
+        return y, x
+
+    def predict(self, state, action):
+        # TODO
+        # y = state[3:6]
+        y, self.x = self._predict(self.x, action)
+        
+        theta = state[2]
+        c, s = np.cos(theta)[0], np.sin(theta)[0]
+        M = np.array([[c, s, 0], [-s, c, 0], [0, 0, 1]])
+
+        delta = np.dot(M, state[3:6].reshape(3, 1)) * self.timestep
+        # print "(x, y, theta): ({}, {}, {}) => ({}, {}, {})".format(state[0], state[1], state[2], delta[0], delta[1], delta[2])
+        state[0:3] += delta
+        state[3:6] = y[:]
+
+        return state
+
+    def reset(self, state):
+        # state: [x, y, theta, x', y', theta']
+        # extract the last 3 elements from state
+        y0 = state[3:6].reshape(3, 1)
+        self.x = np.dot(np.linalg.pinv(self.C), y0)
+
+def build_shared_network(state, add_summaries=False):
+    """
+    Builds a 3-layer network conv -> conv -> fc as described
+    in the A3C paper. This network is shared by both the policy and value net.
+    Args:
+    state: Input state contains rewards, vehicle_state, prev_reward, prev_action
+    add_summaries: If true, add layer summaries to Tensorboard.
+    Returns:
+    Final layer activations.
+    """
+
+    X = state["rewards"]
+
+    # Three convolutional layers
+    conv1 = tf.contrib.layers.conv2d(
+        X, 16, 8, 4, activation_fn=tf.nn.relu, scope="conv1")
+    conv2 = tf.contrib.layers.conv2d(
+        conv1, 32, 4, 2, activation_fn=tf.nn.relu, scope="conv2")
+
+    # Fully connected layer
+    fc1 = tf.contrib.layers.fully_connected(
+        inputs=tf.contrib.layers.flatten(conv2),
+        num_outputs=256,
+        scope="fc1")
+
+    concat1 = tf.concat(1, [fc1, state["prev_reward"]])
+
+    fc2 = tf.contrib.layers.fully_connected(
+        inputs=tf.contrib.layers.flatten(concat1),
+        num_outputs=256,
+        scope="fc2")
+
+    concat2 = tf.concat(1, [fc1, fc2, state["vehicle_state"], state["prev_action"]])
+
+    if add_summaries:
+        tf.contrib.layers.summarize_activation(conv1)
+        tf.contrib.layers.summarize_activation(conv2)
+        tf.contrib.layers.summarize_activation(fc1)
+        tf.contrib.layers.summarize_activation(fc2)
+        tf.contrib.layers.summarize_activation(concat1)
+        tf.contrib.layers.summarize_activation(concat2)
+
+    return concat2
+
+def get_state_placeholder():
+    # rewards' shape: (B, H, W, C)
+    rewards = tf.placeholder(tf.float32, [None, 40, 40, 1], "rewards")
+    vehicle_state = tf.placeholder(tf.float32, [None, 6], "vehicle_state")
+    prev_action = tf.placeholder(tf.float32, [None, 2], "prev_action")
+    prev_reward = tf.placeholder(tf.float32, [None, 1], "prev_reward")
+
+    return {
+        "rewards": rewards,
+        "vehicle_state": vehicle_state,
+        "prev_action": prev_action,
+        "prev_reward": prev_reward
+    }
+
+class PolicyEstimator():
+    """
+    Policy Function approximator. 
+    """
+
+    def __init__(self, env, reuse=False, learning_rate=0.01, scope="policy_estimator"):
+        with tf.variable_scope(scope):
+            self.state = get_state_placeholder()
+            self.target = tf.placeholder(tf.float32, [None], "target")
+
+        # Graph shared with Value Net
+        with tf.variable_scope("shared", reuse=reuse):
+            shared = build_shared_network(self.state, add_summaries=(not reuse))
+
+        with tf.variable_scope(scope):
+            self.mu, self.sigma = self.policy_network(shared, 2)
+
+            normal_dist = tf.contrib.distributions.Normal(self.mu, self.sigma)
+            self.action = tf.reshape(normal_dist.sample_n(1), (-1, 2))
+
+            # clip action if exceed low/high defined in env.action_space
+            max_a = tf.constant(env.max_a, dtype=tf.float32)
+            min_a = tf.constant(env.min_a, dtype=tf.float32)
+            self.action = tf.maximum(tf.minimum(self.action, max_a), min_a)
+
+            # Loss and train op
+            self.loss = -normal_dist.log_prob(self.action) * self.target
+
+            # Add cross entropy cost to encourage exploration
+            self.loss -= 1e-1 * normal_dist.entropy()
+
+            self.optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+            self.train_op = self.optimizer.minimize(
+                self.loss, global_step=tf.contrib.framework.get_global_step())
+
+    def policy_network(self, input, num_outputs):
+
+        # This is just linear classifier
+        mu = tf.contrib.layers.fully_connected(
+            inputs=tf.expand_dims(input, 0),
+            num_outputs=num_outputs,
+            activation_fn=None,
+            weights_initializer=tf.zeros_initializer)
+        mu = tf.reshape(mu, (num_outputs, -1))
+
+        sigma = tf.contrib.layers.fully_connected(
+            inputs=tf.expand_dims(input, 0),
+            num_outputs=num_outputs,
+            activation_fn=None,
+            weights_initializer=tf.zeros_initializer)
+        sigma = tf.reshape(sigma, (num_outputs, -1))
+
+        # Add 1e-5 exploration to make sure it's stochastic
+        sigma = tf.nn.softplus(sigma) + 1e-5
+
+        return mu, sigma
+
+    def predict(self, state, sess=None):
+        sess = sess or tf.get_default_session()
+        feed_dict = { self.state[k]: state[k] for k in self.state.keys() }
+        return sess.run(self.action, feed_dict).T
+
+    def update(self, state, target, action, sess=None):
+        sess = sess or tf.get_default_session()
+        # state = featurize_state(state)
+        feed_dict = { self.state[k]: state[k] for k in self.state.keys() }
+        feed_dict[self.target] = np.array(target).reshape(-1)
+        feed_dict[self.action] = action.reshape(-1, 2)
+
+        _, loss = sess.run([self.train_op, self.loss], feed_dict)
+        return loss
+
+class ValueEstimator():
+    """
+    Value Function approximator. 
+    """
+
+    def __init__(self, reuse=False, learning_rate=0.1, scope="value_estimator"):
+        with tf.variable_scope(scope):
+            self.state = get_state_placeholder()
+            self.target = tf.placeholder(tf.float32, [None], "target")
+
+        # Graph shared with Value Net
+        with tf.variable_scope("shared", reuse=reuse):
+            shared = build_shared_network(self.state, add_summaries=(not reuse))
+
+        with tf.variable_scope(scope):
+            self.value_estimate = self.value_network(shared)
+            self.loss = tf.squared_difference(self.value_estimate, self.target)
+
+            self.optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+            self.train_op = self.optimizer.minimize(
+                self.loss, global_step=tf.contrib.framework.get_global_step())        
+
+    def value_network(self, input, num_outputs=1):
+        # This is just linear classifier
+        value = tf.contrib.layers.fully_connected(
+            inputs=tf.expand_dims(input, 0),
+            num_outputs=num_outputs,
+            activation_fn=None,
+            weights_initializer=tf.zeros_initializer)
+
+        value = tf.squeeze(value)
+
+        return value
+
+    def predict(self, state, sess=None):
+        sess = sess or tf.get_default_session()
+        feed_dict = { self.state[k]: state[k] for k in self.state.keys() }
+        return sess.run(self.value_estimate, feed_dict)
+
+    def update(self, state, target, sess=None):
+        sess = sess or tf.get_default_session()
+        feed_dict = { self.state[k]: state[k] for k in self.state.keys() }
+        feed_dict[self.target] = np.array(target).reshape(-1)
+        _, loss = sess.run([self.train_op, self.loss], feed_dict)
+        return loss
+
+def actor_critic(env, estimator_policy, estimator_value, num_episodes, discount_factor=1.0):
+    """
+    Actor Critic Algorithm. Optimizes the policy 
+    function approximator using policy gradient.
+
+    Args:
+    env: OpenAI environment.
+    estimator_policy: Policy Function to be optimized 
+    estimator_value: Value function approximator, used as a baseline
+    num_episodes: Number of episodes to run for
+    discount_factor: Time-discount factor
+
+    Returns:
+    An EpisodeStats object with two numpy arrays for episode_lengths and episode_rewards.
+    """
+
+    def form_mdp_state(env, state, prev_action, prev_reward):
+        mdp_state = {
+            "rewards": env.rewards[None, ..., None],
+            "vehicle_state": state.T,
+            "prev_action": prev_action.T,
+            "prev_reward": np.array(prev_reward, dtype=np.float32).reshape((-1, 1))
+        }
+        return mdp_state
+
+    # Keeps track of useful statistics
+    stats = plotting.EpisodeStats(
+        episode_lengths=np.zeros(num_episodes),
+        episode_rewards=np.zeros(num_episodes))    
+
+    Transition = collections.namedtuple("Transition", ["state", "action", "reward", "next_state", "done"])
+
+    for i_episode in range(num_episodes):
+        # Reset the environment and pick the fisrst action
+
+        # set initial state (x, y, theta, x', y', theta')
+        state = np.array([0, 0, 0, 0.1, 10, 1], dtype=np.float32).reshape(6, 1)
+        action = np.array([0, 0], dtype=np.float32).reshape(2, 1)
+        reward = 0
+        env._reset(state)
+
+        episode = []
+
+        # One step in the environment
+        for t in itertools.count():
+
+            env.render()
+
+            # Take a step
+            mdp_state = form_mdp_state(env, state, action, reward)
+            action = estimator_policy.predict(mdp_state)
+            next_state, reward, done, _ = env.step(action)
+            print "reward = \33[34m{}\33[0m".format(reward)
+
+            # Keep track of the transition
+            episode.append(Transition(
+                state=state, action=action, reward=reward, next_state=next_state, done=done))
+
+            # Update statistics
+            stats.episode_rewards[i_episode] += reward
+            stats.episode_lengths[i_episode] = t
+
+            # Calculate TD Target
+            next_mdp_state = form_mdp_state(env, next_state, action, reward)
+            value_next = estimator_value.predict(next_mdp_state)
+            td_target = reward + discount_factor * value_next
+            # print "td_target = {}".format(td_target)
+            td_error = td_target - estimator_value.predict(mdp_state)
+
+            # Update the value estimator
+            estimator_value.update(mdp_state, td_target)
+
+            # Update the policy estimator
+            # using the td error as our advantage estimate
+            estimator_policy.update(mdp_state, td_error, action)
+            action2 = estimator_policy.predict(mdp_state)
+            # print "action = ({}, {}), action2 = ({}, {})".format(action[0], action[1], action2[0], action2[1])
+
+            # Print out which step we're on, useful for debugging.
+            print("Step {} @ Episode {}/{} (\33[33m{:.6f}\33[0m)".format(
+                t, i_episode + 1, num_episodes, stats.episode_rewards[i_episode]))
+
+            if done:
+                break
+
+            state = next_state
+
+    return stats
+
+def main():
+    vehicle_model = VehicleModel()
+    rewards = scipy.io.loadmat("/share/Research/Yamaha/d-irl/td_lambda_example/data.mat")["reward"]
+    env = OffRoadNavEnv(rewards, vehicle_model)
+
+    tf.reset_default_graph()
+
+    global_step = tf.Variable(0, name="global_step", trainable=False)
+    # import ipdb; ipdb.set_trace()
+    policy_estimator = PolicyEstimator(env, learning_rate=0.001)
+    value_estimator = ValueEstimator(reuse=True, learning_rate=0.1)
+
+    with tf.Session() as sess:
+        sess.run(tf.initialize_all_variables())
+        # Note, due to randomness in the policy the number of episodes you need to learn a good
+        # TODO: Sometimes the algorithm gets stuck, I'm not sure what exactly is happening there.
+        stats = actor_critic(env, policy_estimator, value_estimator, 50000, discount_factor=0.95)
+
+if __name__ == '__main__':
+    main()
--- gym_offroad_nav/envs/offroad_nav_env.py
+++ gym_offroad_nav/envs/offroad_nav_env.py
@@ -1,22 +1,96 @@
 import gym
+import numpy as np
 from gym import error, spaces, utils
 from gym.utils import seeding
 
 class OffRoadNavEnv(gym.Env):
-  metadata = {'render.modes': ['human']}
+    metadata = {'render.modes': ['human']}
 
-  def __init__(self):
-  	# TODO
-    pass
+    def __init__(self, rewards, vehicle_model):
+        self.viewer = None
 
-  def _step(self, action):
-  	# TODO
-    pass
+        self.action_space = {
+            # maximum speed = 2500 cm/s = 90 km/hr
+            'v_forward': {'low': 0, 'high': 2500},
 
-  def _reset(self):
-  	# TODO
-    pass
+            # maximum yawrate = +- 360 deg/s
+            'yawrate': {'low': -360, 'high': 360}
+        }
 
-  def _render(self, mode='human', close=False):
-  	# TODO
-    pass
+        self.max_a = np.array([self.action_space['v_forward']['high'], self.action_space['yawrate']['high']]).reshape((1, 2))
+        self.min_a = np.array([self.action_space['v_forward']['low'], self.action_space['yawrate']['low']]).reshape((1, 2))
+
+        # A tf.tensor (or np) containing rewards, we need a constant version and 
+        self.rewards = rewards
+
+        self.vehicle_model = vehicle_model
+
+        self.state = None
+
+    def _step(self, action):
+        ''' Take one step in the environment
+        state is the vehicle state, not the full MDP state including history.
+
+        Parameters
+        ----------
+        action : Numpy array
+            The control input for vehicle. [v_forward, yaw_rate]
+
+        Returns
+        -------
+        Tuple
+            A 4-elements tuple (state, reward, done, info)
+        '''
+        self.state = self.vehicle_model.predict(self.state, action)
+
+        # Y forward, X lateral
+        # ix = -19, -18, ...0, 1, 20, iy = 0, 1, ... 39
+        x, y = self.state[:2, 0]
+        ix, iy = np.floor(self.state[:2, 0] / 0.5).astype(np.int)
+        done = (ix < -19) or (ix > 20) or (iy < 0) or (iy > 39)
+
+        reward = self._bilinear_reward_lookup(x, y, ix, iy)
+        print "(ix, iy) = ({}, {}), done = {}".format(
+            ix, iy, "\33[33mTrue\33[0m" if done else "False")
+
+        # debug info
+        info = {}
+
+        return self.state, reward, done, info
+
+    def _get_reward(self, ix, iy):
+        linear_idx = (40 - 1 - iy) * 40 + (ix + 19)
+        r = self.rewards.flatten()[linear_idx]
+        return r
+
+    def _bilinear_reward_lookup(self, x, y, ix, iy):
+        x0 = int(np.clip(ix, -19, 20))
+        y0 = int(np.clip(iy, 0, 39))
+        x1 = int(np.clip(ix + 1, -19, 20))
+        y1 = int(np.clip(iy + 1, 0, 39))
+
+        f00 = self._get_reward(x0, y0)
+        f01 = self._get_reward(x0, y1)
+        f10 = self._get_reward(x1, y0)
+        f11 = self._get_reward(x1, y1)
+
+        w00 = (1.-x)*(1.-y)
+        w01 = y*(1.-x)
+        w10 = x*(1.-y)
+        w11 = x*y
+        r = f00*w00 + f01*w01 + f10*w10 + f11*w11
+        print "reward[{:.4f}, {:.4f}] = {:.4f}*{:.4f} + {:.4f}*{:.4f} + {:.4f}*{:.4f} + {:.4f}*{:.4f} = {}".format(
+            x, y, f00, w00, f01, w01, f10, w10, f11, w11, r
+        ),
+        return r
+
+    def _reset(self, s0):
+        # TODO
+        # Clear any internal state variables, and return the initial state of MDP
+        self.state = s0
+        self.vehicle_model.reset(s0)
+        return s0
+
+    def _render(self, mode='human', close=False):
+        # TODO
+        pass
