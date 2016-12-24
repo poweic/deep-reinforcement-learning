Fixed wrong units bug and use frontview as state

a3c/estimators.py
@@ -3,15 +3,20 @@ import tensorflow as tf
 from tensorflow_helnet.layers import DenseLayer, Conv2DLayer
 
 def get_state_placeholder():
+    # Note that placeholder are tf.Tensor not tf.Variable
+    map_with_vehicle = tf.placeholder(tf.float32, [None, 20, 20, 1], "map_with_vehicle")
     vehicle_state = tf.placeholder(tf.float32, [None, 6], "vehicle_state")
     prev_action = tf.placeholder(tf.float32, [None, 2], "prev_action")
     prev_reward = tf.placeholder(tf.float32, [None, 1], "prev_reward")
 
-    return {
+    state = {
+        "map_with_vehicle": map_with_vehicle,
         "vehicle_state": vehicle_state,
         "prev_action": prev_action,
         "prev_reward": prev_reward
     }
+
+    return state
 
 def build_shared_network(rewards, state, add_summaries=False):
     """
@@ -23,8 +28,19 @@ def build_shared_network(rewards, state, add_summaries=False):
     Returns:
     Final layer activations.
     """
-
     X = rewards
+
+    '''
+    broadcaster = tf.reshape(state["prev_reward"] * 0, [-1, 1, 1, 1])
+    X += broadcaster
+
+    x = state["vehicle_state"][:, 0]
+    y = state["vehicle_state"][:, 1]
+    ix = tf.to_int32(tf.floor(x / 0.5))
+    iy = tf.to_int32(tf.floor(y / 0.5))
+    linear_idx = (40 - 1 - iy) * 40 + (ix + 19)
+    ch2 = tf.zeros_like(X)
+    '''
 
     # Three convolutional layers
     conv1 = tf.contrib.layers.conv2d(
@@ -74,9 +90,9 @@ class PolicyEstimator():
     Policy Function approximator. 
     """
 
-    def __init__(self, rewards, reuse=False, learning_rate=0.001, scope="policy_estimator"):
+    def __init__(self, reuse=False, learning_rate=0.001, scope="policy_estimator"):
 
-        self.rewards = rewards
+        # self.rewards = rewards
 
         with tf.variable_scope(scope):
             self.target = tf.placeholder(tf.float32, [None, 1], "target")
@@ -84,15 +100,17 @@ class PolicyEstimator():
         # Graph shared with Value Net
         with tf.variable_scope("shared", reuse=reuse):
             self.state = get_state_placeholder()
-            shared = build_shared_network(self.rewards, self.state, add_summaries=(not reuse))
+            shared = build_shared_network(self.state["map_with_vehicle"], self.state, add_summaries=(not reuse))
 
         with tf.variable_scope(scope):
             self.mu, self.sigma = self.policy_network(shared, 2)
 
             FLAGS = tf.flags.FLAGS
-            max_a = tf.constant([[FLAGS.max_forward_speed, FLAGS.max_yaw_rate]], dtype=tf.float32)
-            min_a = tf.constant([[FLAGS.min_forward_speed, FLAGS.min_yaw_rate]], dtype=tf.float32)
+            max_a = tf.constant([[FLAGS.max_forward_speed, FLAGS.max_yaw_rate]], dtype=tf.float32) * FLAGS.timestep
+            min_a = tf.constant([[FLAGS.min_forward_speed, FLAGS.min_yaw_rate]], dtype=tf.float32) * FLAGS.timestep
+            '''
             self.mu = (max_a + min_a) / 2 + tf.nn.tanh(self.mu) * (max_a - min_a) / 2
+            '''
             # self.mu = tf.nn.sigmoid(self.mu) * (max_a - min_a) + min_a
 
             self.mu = tf.reshape(self.mu, [-1])
@@ -161,7 +179,8 @@ class PolicyEstimator():
         sigma = tf.reshape(sigma, [-1, 2])
 
         # Add 1e-5 exploration to make sure it's stochastic
-        sigma = tf.nn.sigmoid(sigma) * 5 + 0.1
+        # sigma = tf.nn.sigmoid(sigma) * 5 + 0.1
+        sigma = tf.nn.softplus(sigma) + 0.1
 
         return mu, sigma
 
@@ -185,15 +204,15 @@ class ValueEstimator():
     Value Function approximator. 
     """
 
-    def __init__(self, rewards, reuse=False, learning_rate=0.001, scope="value_estimator"):
-        self.rewards = rewards
+    def __init__(self, state, reuse=False, learning_rate=0.001, scope="value_estimator"):
+        # self.rewards = rewards
         with tf.variable_scope(scope):
             self.target = tf.placeholder(tf.float32, [None, 1], "target")
 
         # Graph shared with Value Net
         with tf.variable_scope("shared", reuse=reuse):
-            self.state = get_state_placeholder()
-            shared = build_shared_network(self.rewards, self.state, add_summaries=(not reuse))
+            self.state = state
+            shared = build_shared_network(self.state["map_with_vehicle"], self.state, add_summaries=(not reuse))
 
         with tf.variable_scope(scope):
             self.logits = self.value_network(shared)

a3c/policy_monitor.py
@@ -48,7 +48,7 @@ class PolicyMonitor(object):
 
         # Local policy net
         with tf.variable_scope("policy_eval"):
-            self.policy_net = PolicyEstimator(self.rewards)
+            self.policy_net = PolicyEstimator()
 
         # Op to copy params from global policy/value net parameters
         self.copy_params_op = make_copy_params_op(
@@ -59,7 +59,8 @@ class PolicyMonitor(object):
         # self.env.monitor.start(directory=self.video_dir, video_callable=lambda x: True, resume=True)
 
     def reset_env(self):
-        self.state = np.array([+7, 10, 0, 0, 20, 1.5], dtype=np.float32).reshape(6, 1)
+        # self.state = np.array([+7, 10, 0, 0, 20, 1.5], dtype=np.float32).reshape(6, 1)
+        self.state = np.array([+9, 1, 0, 0, 20, 0], dtype=np.float32).reshape(6, 1)
         self.action = np.array([0, 0], dtype=np.float32).reshape(2, 1)
         self.env._reset(self.state)
 
@@ -76,7 +77,7 @@ class PolicyMonitor(object):
         reward = 0
 
         while not done:
-            mdp_state = form_mdp_state(self.state, self.action, reward)
+            mdp_state = form_mdp_state(self.env, self.state, self.action, reward)
             action = self.policy_net.predict(mdp_state, sess).reshape(2, -1)
             next_state, reward, done, _ = self.env.step(action)
             total_reward += reward

a3c/train.py
@@ -32,10 +32,11 @@ tf.flags.DEFINE_integer("max_global_steps", None, "Stop training after this many
 tf.flags.DEFINE_integer("eval_every", 300, "Evaluate the policy every N seconds")
 tf.flags.DEFINE_boolean("reset", False, "If set, delete the existing model directory and start training from scratch.")
 tf.flags.DEFINE_integer("parallelism", 6, "Number of threads to run. If not set we run [num_cpu_cores] threads.")
-tf.flags.DEFINE_float("max_forward_speed", 2500 / 10, "Maximum forward velocity of vehicle")
-tf.flags.DEFINE_float("min_forward_speed", 10, "Maximum forward velocity of vehicle")
-tf.flags.DEFINE_float("max_yaw_rate", 3, "Maximum yaw rate (omega) of vehicle")
-tf.flags.DEFINE_float("min_yaw_rate", -3, "Maximum yaw rate (omega) of vehicle")
+tf.flags.DEFINE_float("timestep", 0.01, "Maximum forward velocity of vehicle")
+tf.flags.DEFINE_float("max_forward_speed", 25, "Maximum forward velocity of vehicle (m/s)")
+tf.flags.DEFINE_float("min_forward_speed", 1, "Minimum forward velocity of vehicle (m/s)")
+tf.flags.DEFINE_float("max_yaw_rate", 360, "Maximum yaw rate (omega) of vehicle (degree / sec)")
+tf.flags.DEFINE_float("min_yaw_rate", -360, "Minimum yaw rate (omega) of vehicle (degree / sec)")
 
 FLAGS = tf.flags.FLAGS
 
@@ -50,8 +51,9 @@ cv2.imshow4 = imshow4
 
 def make_env(name=None):
     global envs
-    vehicle_model = VehicleModel()
-    rewards = scipy.io.loadmat("data/circle2.mat")["reward"].astype(np.float32) - 100
+    vehicle_model = VehicleModel(FLAGS.timestep)
+    # rewards = scipy.io.loadmat("data/circle2.mat")["reward"].astype(np.float32) - 100
+    rewards = scipy.io.loadmat("data/maze.mat")["reward"].astype(np.float32) - 15
     env = OffRoadNavEnv(rewards, vehicle_model)
     if name is not None:
         envs.append([name, env])
@@ -82,8 +84,8 @@ with tf.device("/cpu:0"):
     # Global policy and value nets
     with tf.variable_scope("global") as vs:
         rewards = tf.Variable(tf.zeros([1, 40, 40, 1]), name="rewards", trainable=False)
-        policy_net = PolicyEstimator(rewards)
-        value_net = ValueEstimator(rewards, reuse=True)
+        policy_net = PolicyEstimator()
+        value_net = ValueEstimator(policy_net.state, reuse=True)
 
     # Global step iterator
     global_counter = itertools.count()
@@ -148,8 +150,10 @@ with tf.Session() as sess:
         worker_threads.append(t)
 
     # Start a thread for policy eval task
+    '''
     monitor_thread = threading.Thread(target=lambda: pe.continuous_eval(FLAGS.eval_every, sess, coord))
     monitor_thread.start()
+    '''
 
     # Show how agent behaves in envs in main thread
     while True:

a3c/worker.py
@@ -1,6 +1,7 @@
 import gym
 import sys
 import os
+import cv2
 import itertools
 import collections
 import numpy as np
@@ -18,8 +19,38 @@ if import_path not in sys.path:
 from estimators import ValueEstimator, PolicyEstimator
 
 Transition = collections.namedtuple("Transition", ["state", "action", "reward", "next_state", "done"])
-def form_mdp_state(state, prev_action, prev_reward):
+def get_map_with_vehicle(env, state):
+
+    # Rotate the map according the vehicle orientation in degree (not radian)
+    img = env.get_front_view()
+    return img.reshape(1, 20, 20, 1)
+
+    '''
+    rewards = env.rewards #.reshape(1, 40, 40, 1)
+    x, y, theta = state[:3, 0]
+    ix, iy = env.get_ixiy(x, y)
+    linear_idx = env.get_linear_idx(ix, iy)
+
+
+    vehicle = np.zeros_like(rewards).flatten()
+    linear_idx = np.clip(linear_idx, 0, len(vehicle)-1)
+    vehicle[linear_idx] = 1
+    vehicle = vehicle.reshape(rewards.shape)
+
+    rewards = rewards[None, ..., None]
+    vehicle = vehicle[None, ..., None]
+
+    map_with_vehicle = np.concatenate([rewards, vehicle], axis=3)
+
+    return map_with_vehicle
+    '''
+
+def form_mdp_state(env, state, prev_action, prev_reward):
+
+    map_with_vehicle = get_map_with_vehicle(env, state)
+
     return {
+        "map_with_vehicle": map_with_vehicle,
         "vehicle_state": state.T,
         "prev_action": prev_action.T,
         "prev_reward": np.array(prev_reward, dtype=np.float32).reshape((-1, 1))
@@ -83,8 +114,8 @@ class Worker(object):
 
         # Create local policy/value nets that are not updated asynchronously
         with tf.variable_scope(name):
-            self.policy_net = PolicyEstimator(self.rewards)
-            self.value_net = ValueEstimator(self.rewards, reuse=True)
+            self.policy_net = PolicyEstimator()
+            self.value_net = ValueEstimator(self.policy_net.state, reuse=True)
 
         # Op to copy params from global policy/valuenets
         self.copy_params_op = make_copy_params_op(
@@ -123,7 +154,8 @@ class Worker(object):
                 return
 
     def reset_env(self):
-        self.state = np.array([+7, 10, 0, 0, 20, 1.5], dtype=np.float32).reshape(6, 1)
+        # self.state = np.array([+7, 10, 0, 0, 20, 1.5], dtype=np.float32).reshape(6, 1)
+        self.state = np.array([+9, 1, 0, 0, 20, 0], dtype=np.float32).reshape(6, 1)
         self.action = np.array([0, 0], dtype=np.float32).reshape(2, 1)
         self.env._reset(self.state)
 
@@ -138,16 +170,12 @@ class Worker(object):
         total_return = 0
         for i in range(n):
 
-            '''
-            self.env._render({
-                "worker_name": self.name,
-                "max_return": max_return,
-                "total_return": total_return
-            })
-            '''
-
-            mdp_state = form_mdp_state(self.state, self.action, reward)
+            mdp_state = form_mdp_state(self.env, self.state, self.action, reward)
             action = self.policy_net.predict(mdp_state, sess).reshape(2, -1)
+            '''
+            action[0, 0] = 0.05
+            action[1, 0] = 1 / 180. * np.pi
+            '''
             # print "predicted action = {}".format(action.shape)
 
             # Take a step
@@ -190,7 +218,7 @@ class Worker(object):
         reward = 0.0
         last = transitions[-1]
         if not last.done:
-            mdp_state = form_mdp_state(last.next_state, last.action, last.reward)
+            mdp_state = form_mdp_state(self.env, last.next_state, last.action, last.reward)
             # print "mdp_state['prev_action'].shape = {}".format(mdp_state["prev_action"].shape)
             reward = self.value_net.predict(mdp_state, sess)
 
@@ -202,6 +230,8 @@ class Worker(object):
         prev_actions = []
         prev_rewards = []
 
+        mdp_states = []
+
         T = len(transitions)
         for t in range(T)[::-1]:
             transition = transitions[t]
@@ -209,43 +239,39 @@ class Worker(object):
             # Get previous action and reward: a_{t-1}, r_{t-1}
             a_tm1 = transitions[t-1].action if t > 0 else np.zeros((2, 1))
             r_tm1 = transitions[t-1].reward if t > 0 else 0
-            mdp_state = form_mdp_state(transition.state, a_tm1, r_tm1)
+            mdp_state = form_mdp_state(self.env, transition.state, a_tm1, r_tm1)
 
             # Get td_target (reward) and td_error
             reward = transition.reward + self.discount_factor * reward
             td_error = (reward - self.value_net.predict(mdp_state, sess))
 
             # Accumulate updates
-            states.append(transition.state)
-            prev_actions.append(a_tm1)
-            prev_rewards.append(r_tm1)
+            mdp_states.append(mdp_state)
+
             actions.append(transition.action)
             policy_targets.append(td_error)
             value_targets.append(reward)
 
+        # Turn list of dictionaries to dictionary of lists (numpy array)
+        mdp_states = {
+            key: np.squeeze(np.array([s[key] for s in mdp_states]), axis=1)
+            for key in mdp_state.keys()
+        }
+
+        # Add TD Target, TD Error, and action to feed_dict
         feed_dict = {
-            # MDP States
-            self.policy_net.state["vehicle_state"]: np.array(states).reshape((-1, 6)),
-            self.policy_net.state["prev_action"]: np.array(prev_actions).reshape((-1, 2)),
-            self.policy_net.state["prev_reward"]: np.array(prev_rewards).reshape((-1, 1)),
-            # MDP States
-            self.value_net.state["vehicle_state"]: np.array(states).reshape((-1, 6)),
-            self.value_net.state["prev_action"]: np.array(prev_actions).reshape((-1, 2)),
-            self.value_net.state["prev_reward"]: np.array(prev_rewards).reshape((-1, 1)),
-            # TD Target, TD Error, and action
             self.policy_net.target: np.array(policy_targets).reshape((-1, 1)),
             self.policy_net.action: np.array(actions).reshape((-1, 2)),
             self.value_net.target: np.array(value_targets).reshape((-1, 1)),
         }
 
-        '''
+        for k in mdp_states.keys():
+            feed_dict[self.policy_net.state[k]] = mdp_states[k]
+
         for k, v in feed_dict.iteritems():
-            print k, v.shape
-        '''
+            print k.name, v.shape
 
         # Train the global estimators using local gradients
-        # global_step, pnet_loss, vnet_loss, _, _, pnet_summaries, vnet_summaries = sess.run([
-        # print "\33[33m Update policy net and value net\33[0m"
         global_step, pnet_loss, vnet_loss, _, _, pnet_summaries, vnet_summaries = sess.run([
             self.global_step,
             self.policy_net.loss,

gym_offroad_nav/envs/offroad_nav_env.py
@@ -19,6 +19,8 @@ class OffRoadNavEnv(gym.Env):
 
         self.state = None
 
+        self.front_view_disp = np.zeros((400, 400, 3), np.uint8)
+
     def _step(self, action):
         ''' Take one step in the environment
         state is the vehicle state, not the full MDP state including history.
@@ -52,8 +54,12 @@ class OffRoadNavEnv(gym.Env):
 
         return self.state, reward, done, info
 
-    def _get_reward(self, ix, iy):
+    def get_linear_idx(self, ix, iy):
         linear_idx = (40 - 1 - iy) * 40 + (ix + 19)
+        return linear_idx
+
+    def _get_reward(self, ix, iy):
+        linear_idx = self.get_linear_idx(ix, iy)
         r = self.rewards.flatten()[linear_idx]
         return r
 
@@ -98,14 +104,14 @@ class OffRoadNavEnv(gym.Env):
             self.R = self.to_image(self.rewards, self.K)
             self.bR = self.to_image(self.debug_bilinear_R(self.K), self.K)
 
-        self.state = s0
         self.disp_img = np.copy(self.bR)
         self.vehicle_model.reset(s0)
+        self.state = s0
         return s0
 
-    def to_image(self, R, K):
+    def to_image(self, R, K, interpolation=cv2.INTER_NEAREST):
         R = np.clip((R - np.min(R)) * 255. / (np.max(R) - np.min(R)), 0, 255).astype(np.uint8)
-        R = cv2.resize(R, (40*K, 40*K), interpolation=cv2.INTER_NEAREST)[..., None]
+        R = cv2.resize(R, (400, 400), interpolation=cv2.INTER_NEAREST)[..., None]
         R = np.concatenate([R, R, R], axis=2)
         return R
 
@@ -120,6 +126,31 @@ class OffRoadNavEnv(gym.Env):
                 bR[-j, i] = self._bilinear_reward_lookup(x, y, debug=False)
 
         return bR
+
+    def get_front_view(self):
+        x, y, theta = self.state[:3, 0]
+        ix, iy = self.get_ixiy(x, y)
+
+        iix = np.clip(ix + 19, 0, 39)
+        iiy = np.clip(40 - 1 - iy, 0, 39)
+
+        theta *= 180. / np.pi
+        # print "iix = {}, iiy = {}, theta = {}".format(iix, iiy, theta)
+        M = cv2.getRotationMatrix2D((iix, iiy), theta, 1)
+        rotated = cv2.warpAffine(self.rewards, M, (40, 40))
+        rotated = cv2.copyMakeBorder(rotated, 20, 0, 10, 10, 0)
+        img = rotated[iiy:iiy+20, iix:iix+20]
+
+        assert img.shape == (20, 20), "rotated.shape = {}, iix = {}, iiy = {}, img.shape = {}".format(rotated.shape, iix, iiy, img.shape)
+
+        front_view = self.to_image(img, self.K * 2, None)
+        front_view[0, :, :] = 255
+        front_view[-1, :, :] = 255
+        front_view[:, 1, :] = 255
+        front_view[:, -1, :] = 255
+        self.front_view_disp = front_view
+
+        return img
 
     def _render(self, info, mode='human', close=False):
 
@@ -147,6 +178,7 @@ class OffRoadNavEnv(gym.Env):
             # cv2.putText(disp_img, text, (0, 390), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1)
 
             cv2.imshow4(int(wnd_name[-1]), disp_img)
+            cv2.imshow4(int(wnd_name[-1]) + 1, self.front_view_disp)
         '''
         else:
             print "\33[93m{} not initialized yet\33[0m".format(wnd_name)

gym_offroad_nav/vehicle_model.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class VehicleModel():
 
-    def __init__(self, timestep=0.001):
+    def __init__(self, timestep=0.005):
         model = scipy.io.loadmat("../vehicle_modeling/vehicle_model_ABCD.mat")
         self.A = model["A"]
         self.B = model["B"]
@@ -13,6 +13,13 @@ class VehicleModel():
 
         print "A: {}, B: {}, C: {}, D: {}".format(
             self.A.shape, self.B.shape, self.C.shape, self.D.shape)
+
+        # Turn cm/s, degree/s to m/s and rad/s
+        Q = np.diag([100., 180./np.pi]).astype(np.float32)
+        Qinv = np.diag([0.01, 0.01, np.pi/180.]).astype(np.float32)
+        self.B = self.B.dot(Q)
+        self.C = Qinv.dot(self.C)
+        self.D = Qinv.dot(self.D).dot(Q)
 
         # x is the unobservable hidden state, y is the observation
         # u is (v_forward, yaw_rate), y is (vx, vy, w), where
@@ -41,7 +48,9 @@ class VehicleModel():
         # y = state[3:6]
         y, self.x = self._predict(self.x, action)
         
+        # theta is in radian
         theta = state[2]
+
         c, s = np.cos(theta)[0], np.sin(theta)[0]
         M = np.array([[c, s, 0], [-s, c, 0], [0, 0, 1]])
 
@@ -57,4 +66,3 @@ class VehicleModel():
         # extract the last 3 elements from state
         y0 = state[3:6].reshape(3, 1)
         self.x = np.dot(np.linalg.pinv(self.C), y0)
-