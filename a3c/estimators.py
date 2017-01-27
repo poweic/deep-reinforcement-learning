6fc0d836bc2760e7b5479f4d7ac57dc5c42ba7a6
Po-Wei Chou <poweic@andrew.cmu.edu>
2017-01-26 21:14:06 -0500

    Refactoring: move functions around

--- a3c/estimators.py
+++ a3c/estimators.py
@@ -1,6 +1,7 @@
 import numpy as np
 import tensorflow as tf
 from tensorflow_helnet.layers import DenseLayer, Conv2DLayer, MaxPool2DLayer
+from a3c.utils import put_kernels_on_grid, clip, softclip, tf_print, AttrDict
 FLAGS = tf.flags.FLAGS
 batch_size = None
 
@@ -20,71 +21,146 @@ def get_state_placeholder():
 
     return state
 
-def put_kernels_on_grid(kernel, pad = 1):
-
-    '''Visualize conv. features as an image (mostly for the 1st layer).
-    Place kernel into a grid, with some paddings between adjacent filters.
+def build_shared_network(state, add_summaries=False):
+    """
+    Builds a 3-layer network conv -> conv -> fc as described
+    in the A3C paper. This network is shared by both the policy and value net.
     Args:
-      kernel:            tensor of shape [Y, X, NumChannels, NumKernels]
-      (grid_Y, grid_X):  shape of the grid. Require: NumKernels == grid_Y * grid_X
-                           User is responsible of how to break into two multiples.
-      pad:               number of black pixels around each filter (between them)
-    Return:
-      Tensor of shape [(Y+2*pad)*grid_Y, (X+2*pad)*grid_X, NumChannels, 1].
-    '''
-    # get shape of the grid. NumKernels == grid_Y * grid_X
-    def factorize(n):
-        for i in range(int(np.sqrt(float(n))), 0, -1):
-            if n % i == 0:
-                if i == 1: print('Who would enter a prime number of filters?')
-                return (i, int(n / i))
+    add_summaries: If true, add layer summaries to Tensorboard.
+    Returns:
+    Final layer activations.
+    """
 
-    (grid_Y, grid_X) = factorize(kernel.get_shape()[3].value)
-    # print 'grid: %d = (%d, %d)' % (kernel.get_shape()[3].value, grid_Y, grid_X)
+    front_view = state["front_view"]
+    vehicle_state = state["vehicle_state"]
+    prev_action = state["prev_action"]
+    prev_reward = state["prev_reward"]
 
-    # pad X and Y
-    x1 = tf.pad(kernel, tf.constant( [[pad,pad],[pad, pad],[0,0],[0,0]] ), mode = 'CONSTANT')
+    input = front_view
 
-    # X and Y dimensions, w.r.t. padding
-    Y = kernel.get_shape()[0] + 2 * pad
-    X = kernel.get_shape()[1] + 2 * pad
+    with tf.name_scope("conv"):
+        conv1 = Conv2DLayer(input, 32, 3, dilation=1, pad=1, nonlinearity="relu", name="conv1")
+        conv2 = Conv2DLayer(conv1, 32, 3, dilation=2, pad=2, nonlinearity="relu", name="conv2")
+        conv3 = Conv2DLayer(conv2, 32, 3, dilation=4, pad=4, nonlinearity="relu", name="conv3")
+        conv4 = Conv2DLayer(conv3, 32, 3, dilation=8, pad=8, nonlinearity="relu", name="conv4")
+        pool1 = MaxPool2DLayer(conv4, pool_size=3, stride=2, name='pool1')
+        pool2 = MaxPool2DLayer(pool1, pool_size=3, stride=2, name='pool2')
+        '''
+        # Three convolutional layers
+        conv1 = tf.contrib.layers.conv2d(
+            input, 128, 3, 2, activation_fn=tf.nn.relu, scope="conv1")
+        conv2 = tf.contrib.layers.conv2d(
+            conv1, 128, 3, 2, activation_fn=tf.nn.relu, scope="conv2")
+        conv3 = tf.contrib.layers.conv2d(
+            conv2, 128, 3, 2, activation_fn=tf.nn.relu, scope="conv3")
+        '''
 
-    channels = kernel.get_shape()[2]
+    with tf.name_scope("dense"):
+        # Fully connected layer
+        fc1 = DenseLayer(
+            input=tf.contrib.layers.flatten(pool2),
+            num_outputs=256,
+            nonlinearity="relu",
+            name="fc1")
 
-    # put NumKernels to the 1st dimension
-    x2 = tf.transpose(x1, (3, 0, 1, 2))
-    # organize grid on Y axis
-    x3 = tf.reshape(x2, tf.pack([grid_X, Y * grid_Y, X, channels]))
+        concat1 = tf.concat(1, [fc1, prev_reward, vehicle_state, prev_action])
 
-    # switch X and Y axes
-    x4 = tf.transpose(x3, (0, 2, 1, 3))
-    # organize grid on X axis
-    x5 = tf.reshape(x4, tf.pack([1, X * grid_X, Y * grid_Y, channels]))
+        fc2 = DenseLayer(
+            input=concat1,
+            num_outputs=256,
+            nonlinearity="relu",
+            name="fc2")
 
-    # back to normal order (not combining with the next step for clarity)
-    x6 = tf.transpose(x5, (2, 1, 3, 0))
+        concat2 = tf.concat(1, [fc1, fc2, prev_reward, vehicle_state, prev_action])
 
-    # to tf.image_summary order [batch_size, height, width, channels],
-    #   where in this case batch_size == 1
-    x7 = tf.transpose(x6, (3, 0, 1, 2))
+        fc3 = DenseLayer(
+            input=concat2,
+            num_outputs=256,
+            nonlinearity="relu",
+            name="fc3")
 
-    # scaling to [0, 255] is not necessary for tensorboard
-    return x7
+        concat3 = fc3
+        # concat3 = tf.concat(1, [fc1, fc2, fc3, prev_reward, vehicle_state, prev_action])
 
-def clip(x, min_v, max_v):
-    return tf.maximum(tf.minimum(x, max_v), min_v)
+    if add_summaries:
+        with tf.name_scope("summaries"):
+            conv1_w = [v for v in tf.trainable_variables() if "conv1/weights"][0]
+            grid = put_kernels_on_grid(conv1_w)
+            tf.summary.image("conv1/weights", grid)
 
-def softclip(x, min_v, max_v):
-    return (max_v + min_v) / 2 + tf.nn.tanh(x) * (max_v - min_v) / 2
+            tf.summary.image("front_view", front_view, max_outputs=100)
 
-def tf_print(x, message):
-    if not FLAGS.debug:
-        return x
+            tf.contrib.layers.summarize_activation(conv1)
+            tf.contrib.layers.summarize_activation(conv2)
+            tf.contrib.layers.summarize_activation(fc1)
+            tf.contrib.layers.summarize_activation(fc2)
+            tf.contrib.layers.summarize_activation(fc3)
+            tf.contrib.layers.summarize_activation(concat1)
+            tf.contrib.layers.summarize_activation(concat2)
 
-    step = tf.contrib.framework.get_global_step()
-    cond = tf.equal(tf.mod(step, 1), 0)
-    message = "\33[93m" + message + "\33[0m"
-    return tf.cond(cond, lambda: tf.Print(x, [x], message=message, summarize=100), lambda: x)
+    return concat3
+
+def policy_network(input, num_outputs):
+
+    input = DenseLayer(
+        input=input,
+        num_outputs=256,
+        nonlinearity="relu",
+        name="policy-input-dense")
+
+    # Linear classifiers for mu and sigma
+    mu = DenseLayer(
+        input=input,
+        num_outputs=num_outputs,
+        name="policy-mu")
+
+    sigma = DenseLayer(
+        input=input,
+        num_outputs=num_outputs,
+        name="policy-sigma")
+
+    with tf.name_scope("mu_sigma_constraints"):
+        min_mu = tf.constant([[FLAGS.min_mu_vf, FLAGS.min_mu_steer]], dtype=tf.float32)
+        max_mu = tf.constant([[FLAGS.max_mu_vf, FLAGS.max_mu_steer]], dtype=tf.float32)
+
+        min_sigma = tf.constant([[FLAGS.min_sigma_vf, FLAGS.min_sigma_steer]], dtype=tf.float32)
+        max_sigma = tf.constant([[FLAGS.max_sigma_vf, FLAGS.max_sigma_steer]], dtype=tf.float32)
+
+        # Clip mu by min and max, use softplus and capping for sigma
+        # mu = clip(mu, min_mu, max_mu)
+        # sigma = tf.minimum(tf.nn.softplus(sigma) + min_sigma, max_sigma)
+        # sigma = tf.nn.sigmoid(sigma) * 1e-20 + max_sigma
+
+        mu = softclip(mu, min_mu, max_mu)
+        sigma = softclip(sigma, min_sigma, max_sigma)
+
+    return mu, sigma
+
+def state_value_network(input, num_outputs=1):
+    """
+    This is state-only value V
+    """
+    input = DenseLayer(
+        input=input,
+        num_outputs=256,
+        nonlinearity="relu",
+        name="value-input-dense")
+
+    input = DenseLayer(
+        input=input,
+        num_outputs=256,
+        nonlinearity="relu",
+        name="value-input-dense-2")
+
+    # This is just linear classifier
+    value = DenseLayer(
+        input=input,
+        num_outputs=num_outputs,
+        name="value-dense")
+
+    value = tf.reshape(value, [-1, 1])
+
+    return value
 
 class PolicyValueEstimator():
     """
@@ -102,15 +178,15 @@ class PolicyValueEstimator():
             self.actions_ext = tf.placeholder(tf.float32, [batch_size, 2], "actions_ext")
 
         with tf.variable_scope("shared"):
-            shared = self.build_shared_network(add_summaries=add_summaries)
+            shared = build_shared_network(self.state, add_summaries=add_summaries)
 
         with tf.name_scope("policy_network"):
-            self.mu, self.sigma = self.policy_network(shared, 2)
+            self.mu, self.sigma = policy_network(shared, 2)
             normal_dist = self.get_normal_dist(self.mu, self.sigma)
             self.actions = self.sample_actions(normal_dist)
 
-        with tf.name_scope("value_network"):
-            self.logits = self.value_network(shared)
+        with tf.name_scope("state_value_network"):
+            self.logits = state_value_network(shared)
 
         with tf.name_scope("losses"):
             self.pi_loss = self.get_policy_loss(normal_dist)
@@ -150,86 +226,6 @@ class PolicyValueEstimator():
             # ====== DEBUG ======
 
             self.summaries = self.summarize()
-
-    def build_shared_network(self, add_summaries=False):
-        """
-        Builds a 3-layer network conv -> conv -> fc as described
-        in the A3C paper. This network is shared by both the policy and value net.
-        Args:
-        add_summaries: If true, add layer summaries to Tensorboard.
-        Returns:
-        Final layer activations.
-        """
-        state = self.state
-
-        front_view = state["front_view"]
-        vehicle_state = state["vehicle_state"]
-        prev_action = state["prev_action"]
-        prev_reward = state["prev_reward"]
-
-        input = front_view
-
-        with tf.name_scope("conv"):
-            conv1 = Conv2DLayer(input, 32, 3, dilation=1, pad=1, nonlinearity="relu", name="conv1")
-            conv2 = Conv2DLayer(conv1, 32, 3, dilation=2, pad=2, nonlinearity="relu", name="conv2")
-            conv3 = Conv2DLayer(conv2, 32, 3, dilation=4, pad=4, nonlinearity="relu", name="conv3")
-            conv4 = Conv2DLayer(conv3, 32, 3, dilation=8, pad=8, nonlinearity="relu", name="conv4")
-            pool1 = MaxPool2DLayer(conv4, pool_size=3, stride=2, name='pool1')
-            pool2 = MaxPool2DLayer(pool1, pool_size=3, stride=2, name='pool2')
-            '''
-            # Three convolutional layers
-            conv1 = tf.contrib.layers.conv2d(
-                input, 128, 3, 2, activation_fn=tf.nn.relu, scope="conv1")
-            conv2 = tf.contrib.layers.conv2d(
-                conv1, 128, 3, 2, activation_fn=tf.nn.relu, scope="conv2")
-            conv3 = tf.contrib.layers.conv2d(
-                conv2, 128, 3, 2, activation_fn=tf.nn.relu, scope="conv3")
-            '''
-
-        with tf.name_scope("dense"):
-            # Fully connected layer
-            fc1 = DenseLayer(
-                input=tf.contrib.layers.flatten(pool2),
-                num_outputs=256,
-                nonlinearity="relu",
-                name="fc1")
-
-            concat1 = tf.concat(1, [fc1, prev_reward, vehicle_state, prev_action])
-
-            fc2 = DenseLayer(
-                input=concat1,
-                num_outputs=256,
-                nonlinearity="relu",
-                name="fc2")
-
-            concat2 = tf.concat(1, [fc1, fc2, prev_reward, vehicle_state, prev_action])
-
-            fc3 = DenseLayer(
-                input=concat2,
-                num_outputs=256,
-                nonlinearity="relu",
-                name="fc3")
-
-            concat3 = fc3
-            # concat3 = tf.concat(1, [fc1, fc2, fc3, prev_reward, vehicle_state, prev_action])
-
-        if add_summaries:
-            with tf.name_scope("summaries"):
-                conv1_w = [v for v in tf.trainable_variables() if "conv1/weights"][0]
-                grid = put_kernels_on_grid(conv1_w)
-                tf.summary.image("conv1/weights", grid)
-
-                tf.summary.image("front_view", front_view, max_outputs=100)
-
-                tf.contrib.layers.summarize_activation(conv1)
-                tf.contrib.layers.summarize_activation(conv2)
-                tf.contrib.layers.summarize_activation(fc1)
-                tf.contrib.layers.summarize_activation(fc2)
-                tf.contrib.layers.summarize_activation(fc3)
-                tf.contrib.layers.summarize_activation(concat1)
-                tf.contrib.layers.summarize_activation(concat2)
-
-        return concat3
 
     def get_policy_loss(self, normal_dist):
         # policy loss is the negative of log_prob times advantages
@@ -354,65 +350,6 @@ class PolicyValueEstimator():
         steer = tf.atan(FLAGS.wheelbase / radius)
         # steer = tf_print(steer, "steer = ")
         return steer
-
-    def policy_network(self, input, num_outputs):
-
-        input = DenseLayer(
-            input=input,
-            num_outputs=256,
-            nonlinearity="relu",
-            name="policy-input-dense")
-
-        # Linear classifiers for mu and sigma
-        mu = DenseLayer(
-            input=input,
-            num_outputs=num_outputs,
-            name="policy-mu")
-
-        sigma = DenseLayer(
-            input=input,
-            num_outputs=num_outputs,
-            name="policy-sigma")
-
-        with tf.name_scope("mu_sigma_constraints"):
-            min_mu = tf.constant([[FLAGS.min_mu_vf, FLAGS.min_mu_steer]], dtype=tf.float32)
-            max_mu = tf.constant([[FLAGS.max_mu_vf, FLAGS.max_mu_steer]], dtype=tf.float32)
-
-            min_sigma = tf.constant([[FLAGS.min_sigma_vf, FLAGS.min_sigma_steer]], dtype=tf.float32)
-            max_sigma = tf.constant([[FLAGS.max_sigma_vf, FLAGS.max_sigma_steer]], dtype=tf.float32)
-
-            # Clip mu by min and max, use softplus and capping for sigma
-            # mu = clip(mu, min_mu, max_mu)
-            # sigma = tf.minimum(tf.nn.softplus(sigma) + min_sigma, max_sigma)
-            # sigma = tf.nn.sigmoid(sigma) * 1e-20 + max_sigma
-
-            mu = softclip(mu, min_mu, max_mu)
-            sigma = softclip(sigma, min_sigma, max_sigma)
-
-        return mu, sigma
-
-    def value_network(self, input, num_outputs=1):
-        input = DenseLayer(
-            input=input,
-            num_outputs=256,
-            nonlinearity="relu",
-            name="value-input-dense")
-
-        input = DenseLayer(
-            input=input,
-            num_outputs=256,
-            nonlinearity="relu",
-            name="value-input-dense-2")
-
-        # This is just linear classifier
-        value = DenseLayer(
-            input=input,
-            num_outputs=num_outputs,
-            name="value-dense")
-
-        value = tf.reshape(value, [-1, 1])
-
-        return value
 
     def predict(self, state, tensors, sess=None):
         sess = sess or tf.get_default_session()
--- a3c/utils.py
+++ a3c/utils.py
@@ -1,5 +1,7 @@
 import numpy as np
 import scipy.interpolate as interpolate
+import tensorflow as tf
+FLAGS = tf.flags.FLAGS
 
 def inverse_transform_sampling_2d(data, n_samples, version=2):
 
@@ -38,3 +40,77 @@ def inverse_transform_sampling_2d(data, n_samples, version=2):
             ys[i] = inv_y_cdf[x_ind](ry)
 
     return xs.squeeze(), ys.squeeze()
+
+def clip(x, min_v, max_v):
+    return tf.maximum(tf.minimum(x, max_v), min_v)
+
+def softclip(x, min_v, max_v):
+    return (max_v + min_v) / 2 + tf.nn.tanh(x) * (max_v - min_v) / 2
+
+def tf_print(x, message):
+    if not FLAGS.debug:
+        return x
+
+    step = tf.contrib.framework.get_global_step()
+    cond = tf.equal(tf.mod(step, 1), 0)
+    message = "\33[93m" + message + "\33[0m"
+    return tf.cond(cond, lambda: tf.Print(x, [x], message=message, summarize=100), lambda: x)
+
+def multiply(list_a, list_b):
+    return [a*b for a, b in zip(list_a, list_b)]
+
+class AttrDict(dict):
+    def __init__(self, *args, **kwargs):
+        super(AttrDict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
+
+def put_kernels_on_grid(kernel, pad = 1):
+
+    '''Visualize conv. features as an image (mostly for the 1st layer).
+    Place kernel into a grid, with some paddings between adjacent filters.
+    Args:
+      kernel:            tensor of shape [Y, X, NumChannels, NumKernels]
+      (grid_Y, grid_X):  shape of the grid. Require: NumKernels == grid_Y * grid_X
+                           User is responsible of how to break into two multiples.
+      pad:               number of black pixels around each filter (between them)
+    Return:
+      Tensor of shape [(Y+2*pad)*grid_Y, (X+2*pad)*grid_X, NumChannels, 1].
+    '''
+    # get shape of the grid. NumKernels == grid_Y * grid_X
+    def factorize(n):
+        for i in range(int(np.sqrt(float(n))), 0, -1):
+            if n % i == 0:
+                if i == 1: print('Who would enter a prime number of filters?')
+                return (i, int(n / i))
+
+    (grid_Y, grid_X) = factorize(kernel.get_shape()[3].value)
+    # print 'grid: %d = (%d, %d)' % (kernel.get_shape()[3].value, grid_Y, grid_X)
+
+    # pad X and Y
+    x1 = tf.pad(kernel, tf.constant( [[pad,pad],[pad, pad],[0,0],[0,0]] ), mode = 'CONSTANT')
+
+    # X and Y dimensions, w.r.t. padding
+    Y = kernel.get_shape()[0] + 2 * pad
+    X = kernel.get_shape()[1] + 2 * pad
+
+    channels = kernel.get_shape()[2]
+
+    # put NumKernels to the 1st dimension
+    x2 = tf.transpose(x1, (3, 0, 1, 2))
+    # organize grid on Y axis
+    x3 = tf.reshape(x2, tf.pack([grid_X, Y * grid_Y, X, channels]))
+
+    # switch X and Y axes
+    x4 = tf.transpose(x3, (0, 2, 1, 3))
+    # organize grid on X axis
+    x5 = tf.reshape(x4, tf.pack([1, X * grid_X, Y * grid_Y, channels]))
+
+    # back to normal order (not combining with the next step for clarity)
+    x6 = tf.transpose(x5, (2, 1, 3, 0))
+
+    # to tf.image_summary order [batch_size, height, width, channels],
+    #   where in this case batch_size == 1
+    x7 = tf.transpose(x6, (3, 0, 1, 2))
+
+    # scaling to [0, 255] is not necessary for tensorboard
+    return x7
