c71ef6fc71711b3de79a76c9a8f5f07ec25d7540
Po-Wei Chou <poweic@andrew.cmu.edu>
2017-02-10 00:04:15 -0500

    Change to a larger timestep since command_freq is low

--- go.sh
+++ go.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 ./train.py \
   --model-dir /Data3/acer-offroad/ \
-  --parallelism 2 \
-  --game line \
-  --save-every-n-minutes 60 \
-  --t-max 4 \
+  --parallelism 4 \
+  --game s_shape \
+  --save-every-n-minutes 10 \
+  --t-max 20 \
   --max-gradient 100.0 \
-  --field-of-view 40 \
-  --timestep 0.001 \
-  --replay-ratio 8 \
-  --avg-net-momentum 0.995 \
+  --field-of-view 20 \
+  --timestep 0.01 \
+  --replay-ratio 0.125 \
+  --avg-net-momentum 0.95 \
   --max-replay-buffer-size 5000 \
-  --command-freq 10 \
+  --command-freq 5 \
   --learning-rate 1e-3 \
   --discount-factor 0.99 \
   --n-agents-per-worker 8 \
-  --vehicle-model-noise-level 1e-2 \
-  --l2-reg 1e-4 \
+  --vehicle-model-noise-level 2e-2 \
+  --l2-reg 1e-3 \
   $@
