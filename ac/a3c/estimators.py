75af6a4ccc730edc72fded15cde42e469a7168bf
Po-Wei Chou <poweic@andrew.cmu.edu>
2017-03-23 01:18:39 -0400

    Change fixed "2" to FLAGS.num_actions

--- ac/a3c/estimators.py
+++ ac/a3c/estimators.py
@@ -21,7 +21,7 @@ class A3CEstimator():
         with tf.name_scope("outputs"):
             self.advantages  = tf.placeholder(tf.float32, [seq_length, batch_size, 1], "advantages")
             self.returns     = tf.placeholder(tf.float32, [seq_length, batch_size, 1], "returns")
-            self.actions_ext = tf.placeholder(tf.float32, [seq_length, batch_size, 2], "actions_ext")
+            self.actions_ext = tf.placeholder(tf.float32, [seq_length, batch_size, FLAGS.num_actions], "actions_ext")
 
         with tf.variable_scope("shared"):
             shared, self.lstm = build_shared_network(self.state, add_summaries=add_summaries)
